d5c87221383ea8364f2f37aa005d74c263664c87
Mateusz Żłobiński <mateusz.zlobinski@outlook.com>
2021-06-27 17:18:42 +0200

    version alpha 1.32 - added cursors

--- main.py
+++ main.py
@@ -8,24 +8,26 @@ from nptdms import TdmsFile
 import numpy as np
 
 
-# M. Zlobinski - Light TDMS viewer
+# M. Zlobinski - TDMS Easy Viewer
 # Version 1.00
 # Version 1.10 - option to plot multiple lines on one plot
 # Version 1.11 - improved looks
-# Version 1.20 - added custom x axis and statistical infos
+# Version 1.20 - added custom x axis and statistical info
+# Version 1.30 - added filtering capabilities
 
 def fd_plot(filename, clear_plot, xminval, xmaxval):
     ch_num = int(ch_indexEntry.get())
     active_channel = channellist[ch_num]
     active_group = grouplist[ch_num]
-    print(xminval)
-    print(xmaxval)
+    curL=int(cursorL_Entry.get())
+    curR=int(cursorR_Entry.get())
     with TdmsFile.open(filename) as tdms_file:
         active_channel = tdms_file[active_group][active_channel]
         for chunk in active_channel.data_chunks():
             if xmaxval == 0:
                 xmaxval = int(len(active_channel))
             data = chunk[xminval:xmaxval]
+
     stat_info = f'min: {round(min(data), 3)} | max: {round(max(data), 3)} | avg: {round(np.average(data), 3)}'
     statistic_infoLabel.configure(text=stat_info)
     if (clear_plot == 0) and (ax.lines is not None):
@@ -39,6 +41,14 @@ def fd_plot(filename, clear_plot, xminval, xmaxval):
         y_lim_min = -1
         y_lim_max = 1
     ax.set_ylim([y_lim_min, y_lim_max])
+    if curL != curR:
+        curData = data[curL:curR]
+        cur_min = round(min(curData), 3)
+        cur_max = round(max(curData), 3)
+        cur_avg = round(np.average(curData),3)
+        cur_stat_info = f'min: {cur_min} | max: {cur_max} | avg: {cur_avg}'
+        cursor_infoLabel.configure(text=cur_stat_info)
+        ax.vlines([curL, curR], y_lim_min, y_lim_max, color="grey", linestyle="dashed")
     ax.plot(data, label=active_channel.name)
     ax.legend()
     canvas.draw()
@@ -48,6 +58,7 @@ def browse():
     filepathEntry.delete(0, tk.END)
     filepath = filedialog.askopenfilename()
     filepathEntry.insert(0, filepath)
+    filterEntry.insert(0, "<filter>")
 
 
 def get_channels(filename):
@@ -59,13 +70,15 @@ def get_channels(filename):
     index = []
     i = 0
     filter = filterEntry.get()
+    init_cond = False
+    if np.size(filter) == 0: init_cond = True
+    if filter == "<filter>": init_cond = True
     with TdmsFile.open(filename) as tdms_file:  # read file but do not load
         groups = tdms_file.groups()
         for group in groups:
             channels = group.channels()
             for channel in channels:
-
-                if (np.size(filter) == 0) or (filter in channel.name):
+                if (init_cond == True) or (filter in channel.name):
                     index.append(i)
                     channellist.append(channel.name)
                     grouplist.append(group.name)
@@ -80,20 +93,22 @@ def select_channel(event):
     # picked = widget.get(selection[0]) get name
     ch_indexEntry.insert(0, listbox.curselection()[0])
 
+
 def filter_applying(event):
     get_channels(filepathEntry.get())
 
+#--------------------------GUI------------------------------------------------------------------------------------------
 # Main window settings
 root = tk.Tk()
 root.title('TDMS Easy Viewer')
-root.geometry("1000x720")
+root.geometry("1080x720")
 root.resizable(False, False)
 
 # define Frames
 FileInputFrame = Frame(root, bg='silver', height=50, borderwidth=0, highlightthickness=0)
 OptionsFrame = Frame(root, bg="whitesmoke", height=50, borderwidth=0, highlightthickness=0)
 ChListFrame = Frame(root, bg='whitesmoke', borderwidth=0, highlightthickness=0)
-PlotFrame = Frame(root, bg='white', borderwidth=0, highlightthickness=0)
+PlotFrame = Frame(root, bg='whitesmoke', borderwidth=0, highlightthickness=0)
 FooterFrame = Frame(root, bg='grey', borderwidth=0, highlightthickness=0)
 # Distribute Frames
 FileInputFrame.grid(row=0, column=0, sticky="ew", columnspan=2)
@@ -105,7 +120,7 @@ FooterFrame.grid(row=3, column=0, columnspan=2, sticky="sew")
 # define elements - file input frame
 FileInputLabel = Label(FileInputFrame, text="File path:", bg="silver", fg="black")
 browseButton = tk.Button(FileInputFrame, text="browse", padx=20, pady=2, command=browse)
-filepathEntry = tk.Entry(FileInputFrame, width=130)
+filepathEntry = tk.Entry(FileInputFrame, width=140)
 # distribute elements for file input frame
 FileInputLabel.pack(side="left")
 browseButton.pack(side="right", padx=30)
@@ -115,13 +130,23 @@ filepathEntry.pack(side="left", padx=10)
 optionsLabel = Label(OptionsFrame, text="Plotting options:", bg="whitesmoke", fg="black")
 var1 = tk.IntVar()
 keep_graph_cb = tk.Checkbutton(OptionsFrame, text='Add Graph', variable=var1, onvalue=1, offvalue=0)
-statistic_infoLabel = Label(OptionsFrame, bg="whitesmoke", fg="black")
-optionsLabel2 = Label(OptionsFrame, text="|  x min ", bg="whitesmoke", fg="black")
+statistic_infoLabel = Label(OptionsFrame, bg="whitesmoke", fg="blue")
+optionsLabel2 = Label(OptionsFrame, text="  |  xscale  from:", bg="whitesmoke", fg="black")
 xminEntry = tk.Entry(OptionsFrame, width=8)
 xminEntry.insert(0, 0)
-optionsLabel3 = Label(OptionsFrame, text="x max: ", bg="whitesmoke", fg="black")
+
+optionsLabel3 = Label(OptionsFrame, text="to: ", bg="whitesmoke", fg="black")
 xmaxEntry = tk.Entry(OptionsFrame, width=8)
 xmaxEntry.insert(0, 0)
+
+optionsLabel4 = Label(OptionsFrame, text="  |   cursor:  left: ", bg="whitesmoke", fg="black")
+cursorL_Entry = tk.Entry(OptionsFrame, width=8)
+cursorL_Entry.insert(0, 0)
+
+optionsLabel5 = Label(OptionsFrame, text=" right: ", bg="whitesmoke", fg="black")
+cursorR_Entry = tk.Entry(OptionsFrame, width=8)
+cursorR_Entry.insert(0, 0)
+cursor_infoLabel = Label(OptionsFrame, bg="whitesmoke", fg="blue")
 
 # distribute elements in options frame
 optionsLabel.pack(side="left")
@@ -130,7 +155,12 @@ optionsLabel2.pack(side="left")
 xminEntry.pack(side="left")
 optionsLabel3.pack(side="left")
 xmaxEntry.pack(side="left")
-statistic_infoLabel.pack(side="right", padx=30)
+statistic_infoLabel.pack(side="left")
+optionsLabel4.pack(side="left")
+cursorL_Entry.pack(side="left")
+optionsLabel5.pack(side="left")
+cursorR_Entry.pack(side="left")
+cursor_infoLabel.pack(side="left")
 
 # define elements - channel list frame
 c_var = tk.StringVar()
